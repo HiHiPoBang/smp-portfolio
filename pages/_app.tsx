62ef9ccea5a4dc63b04338016193d0c90727bd22
PoBang <hihipobang@gmail.com>
2022-01-28 21:51:28 +0800

    feat: add header in mdx style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import '../styles/globals.css';
 import '../lib/fontawesome';
 import type { AppProps } from 'next/app';
 import { MDXProvider } from '@mdx-js/react';
-import { H1, H2 } from '../components';
+import { H1, H2, H3, H4, H5 } from '../components';
 
 const components = {
   h1: H1,
   h2: H2,
+  h3: H3,
+  h4: H4,
+  h5: H5,
 };
 function MyApp({ Component, pageProps }: AppProps) {
   return (
